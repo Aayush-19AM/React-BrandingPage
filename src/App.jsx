0e9aa33518b8de770ec9969cc068fa8ad7d0e526
Aayush-19AM <84622618+Aayush-19AM@users.noreply.github.com>
2023-12-28 22:53:43 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import HeroSection from "./components/Hero"
+import Navigation from "./components/Navigation"
+function App() {
+    return(
+        <div>
+           <Navigation/>
+           <HeroSection/>
+        </div>
+    )
+}
+export default App
